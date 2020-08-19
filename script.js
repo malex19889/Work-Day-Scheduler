af08f4a0e6b56ea14dfb8f2b2e53acdccad54f12
Michael Willingham <MichaelWillingham1@Michaels-MacBook-Pro.local>
2020-08-18 21:23:07 -0500

    push from mobile workstation added hours description and save button

--- script.js
+++ script.js
@@ -16,9 +16,23 @@ var rowEl = $("<div>");
 rowEl.addClass("row")
 rows.append(rowEl)
 // create hour block
-
+var hour = $("<div>");
+hour.addClass("col-md-2 hour");
+if(hours >12){
+hour.text(hours[i]+"Am");
+rowEl.append(hour)
+}else{
+hour.text(hours[i]+"Pm");
+rowEl.append(hour)
+}
 // create description block
-
+var description = $("<div>");
+description.addClass("col-md-8 present")
+description.text("Hello")
+rowEl.append(description);
 // create savebutton
+var saveBtn = $("<div>");
+saveBtn.addClass("col-md-2 saveBtn")
+rowEl.append(saveBtn)
 }
 
